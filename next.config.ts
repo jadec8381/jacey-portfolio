89abf05bfb187cf58e3fdbee3b70307019ffffa6
Jacey <jadec8381@gmail.com>
2026-04-16 13:47:17 -0400

    navbar更新

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* 🌟 核心：强行忽略所有的类型检查报错 */
+
   typescript: {
     ignoreBuildErrors: true,
   },
-  /* 🌟 核心：强行忽略所有的 ESLint 检查报错 */
+
   eslint: {
     ignoreDuringBuilds: true,
   },
